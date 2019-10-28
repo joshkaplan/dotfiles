9808146337e376d29330c364b514e882df1a1704
joshkaplan <joshkaplan@users.noreply.github.com>
2019-10-28 13:58:42 -0700

    add local git config

---
+++ dotfiles/hammerspoon/layouts.lua
@@ -0,0 +1,105 @@
+--[[
+{
+  "hs.application: Finder (0x608000655d48)"
+  "hs.window:  (0x608000653948)"
+  "hs.screen: LED Cinema Display (0x608000648ea8)"
+  {
+    _x = "-593.0"
+    _w = "2560.0"
+    _h = "2240.0"
+    _y = "-1440.0"
+  }
+}
+{
+  "hs.application: Fantastical (0x6080010441c8)"
+  "hs.window: Fantastical 2 (0x600000e5a578)"
+  "hs.screen: Color LCD (0x6080012577b8)"
+  {
+    _x = "280.0"
+    _w = "361.0"
+    _h = "778.0"
+    _y = "22.0"
+  }
+}
+{
+  "hs.application: Slack (0x6080010570c8)"
+  "hs.window: Slack - Graphiq (0x60800124de58)"
+  "hs.screen: LED Cinema Display (0x600000e42518)"
+  {
+    _x = "-593.0"
+    _w = "1280.0"
+    _h = "1418.0"
+    _y = "-1418.0"
+  }
+}
+{
+  "hs.application: Spark (0x60800124b1e8)"
+  "hs.window: Spark - Code (0x600000e59978)"
+  "hs.screen: LED Cinema Display (0x60800124a5b8)"
+  {
+    _x = "687.0"
+    _w = "1280.0"
+    _h = "1418.0"
+    _y = "-1418.0"
+  }
+}
+{
+  "hs.application: Amazon Chime (0x608000459708)"
+  "hs.window: Amazon Chime (0x6000012562e8)"
+  "hs.screen: LED Cinema Display (0x60800065a9f8)"
+  {
+    _x = "-593.0"
+    _w = "1280.0"
+    _h = "1418.0"
+    _y = "-1418.0"
+  }
+}
+{
+  "hs.application: Sublime Text (0x608000a4bab8)"
+  "hs.window: init.lua (0x600000e58898)"
+  "hs.screen: LED Cinema Display (0x600000c5cb28)"
+  {
+    _x = "-273.0"
+    _w = "1920.0"
+    _h = "1063.0"
+    _y = "-1241.0"
+  }
+}
+
+2018-08-06 08:53:38: 08:53:38      [LOG]: {
+  is_minimized = "false"
+  window = "hs.window:  (0x608000653948)"
+  app = "hs.application: Finder (0x600000e594c8)"
+  is_hidden = "false"
+}
+{
+  is_minimized = "false"
+  window = "hs.window: Fantastical 2 (0x600000e5a578)"
+  app = "hs.application: Fantastical (0x600000659f48)"
+  is_hidden = "false"
+}
+{
+  is_minimized = "false"
+  window = "hs.window: Slack - Graphiq (0x60800124de58)"
+  app = "hs.application: Slack (0x6080004440a8)"
+  is_hidden = "false"
+}
+{
+  is_minimized = "false"
+  window = "hs.window: Spark - Code (0x600000e59978)"
+  app = "hs.application: Spark (0x60800124ff88)"
+  is_hidden = "false"
+}
+{
+  is_minimized = "false"
+  window = "hs.window: Amazon Chime (0x6000012562e8)"
+  app = "hs.application: Amazon Chime (0x6080010464a8)"
+  is_hidden = "false"
+}
+{
+  is_minimized = "false"
+  window = "hs.window: init.lua (0x600000e58898)"
+  app = "hs.application: Sublime Text (0x600000c57a58)"
+  is_hidden = "false"
+}
+--]]
